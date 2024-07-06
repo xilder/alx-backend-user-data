4c2ec16c9f74e3344a0db27ee089cd4c9c7bb129
xilder <olabowaleayobami@gmail.com>
2024-07-06 07:35:21 +0100

    adds task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """obfuscates certain data fields"""
 
-
+import logging
 from typing import List
 import re
 
@@ -17,3 +17,27 @@ def filter_datum(
         message = re.sub(f"{f}=.*?{separator}",
                          f"{f}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> str:
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format method"""
+        message = super(RedactingFormatter, self).format(record)
+        record.msg = filter_datum(
+            self.fields,
+            self.REDACTION,
+            message,
+            self.SEPARATOR
+        )
+        return record.msg
