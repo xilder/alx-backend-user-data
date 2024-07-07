74518fd02106f2e3b5577dfe1109dbac5923e017
xilder <olabowaleayobami@gmail.com>
2024-07-07 15:40:11 +0100

    aa

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -19,3 +19,24 @@ def hash_password(password: str) -> bytes:
     encoded = password.encode()
     hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+        Validates whether the provided password matches the hashed password.
+
+        Args:
+                hashed_password (bytes): A byte string representing
+                the salted, hashed password.
+                password (str): A string containing the plain text
+                password to be validated.
+
+        Returns:
+                bool: True if the provided password matches the hashed
+                password, False otherwise.
+        """
+    valid = False
+    encoded = password.encode()
+    if bcrypt.checkpw(encoded, hashed_password):
+        valid = True
+    return valid
