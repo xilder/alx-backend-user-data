b3292fdd48b67e4348f56b87ba8ecdcd08619c4a
xilder <olabowaleayobami@gmail.com>
2024-07-07 15:38:13 +0100

    adds encrypt password function

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Password Encryption and Validation Module
+"""
+import bcrypt
+
+
+def hash_passwd(password: str) -> bytes:
+    """
+    Generates a salted and hashed password.
+
+    Args:
+            password (str): A string containing the plain text
+            password to be hashed.
+
+    Returns:
+            bytes: A byte string representing the salted, hashed password.
+    """
+    encoded = password.encode()
+    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
+    return hashed
