d04b0c2eb9b875a328187c3acd5bf76e68c84551
xilder <olabowaleayobami@gmail.com>
2024-07-07 07:40:28 +0100

    aa

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,8 +2,12 @@
 """obfuscates certain data fields"""
 
 import logging
+from logging import Logger
 from typing import List
 import re
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(
@@ -17,6 +21,17 @@ def filter_datum(
         message = re.sub(f"{f}=.*?{separator}",
                          f"{f}={redaction}{separator}", message)
     return message
+
+
+def get_logger() -> Logger:
+    """returns a logger object"""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(RedactingFormatter(list(PII_FIELDS)))
+    logger.addHandler(stream_handler)
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
