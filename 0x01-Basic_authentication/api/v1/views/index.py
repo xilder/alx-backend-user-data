d5e7c9b7a9f14efb3851b8f17862374232eefa3a
xilder <olabowaleayobami@gmail.com>
2024-07-08 19:31:49 +0100

    adds task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,6 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/api/v1/unauthorized', strict_slashes=False)
 def unauthorised() -> str:
     """unauthorised route"""
