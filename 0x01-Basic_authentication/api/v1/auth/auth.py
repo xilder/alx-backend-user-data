3ba609589ce9e0d18e2b89118b581ff1502422e4
xilder <olabowaleayobami@gmail.com>
2024-07-08 20:16:38 +0100

    aa

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,15 +12,13 @@ class Auth:
             self,
             path: str,
             excluded_paths: List[str]
-        ) -> bool:
+    ) -> bool:
         """returns False"""
         return False
-
 
     def authorization_header(self, request=None) -> str:
         """returns None"""
         return None
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """"returns None"""
