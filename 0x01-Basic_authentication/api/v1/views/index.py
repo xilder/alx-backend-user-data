272d4a663de0d501c67d655f2823159d1eb695b2
xilder <olabowaleayobami@gmail.com>
2024-07-08 19:40:42 +0100

    updates task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorised() -> str:
     """unauthorised route"""
     abort(401)
