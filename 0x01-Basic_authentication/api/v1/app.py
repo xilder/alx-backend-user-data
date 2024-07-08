37eef10c6c48ca320b13dec99a879044190965d7
xilder <olabowaleayobami@gmail.com>
2024-07-08 19:59:02 +0100

    updates task 1

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -35,7 +35,6 @@ def forbidden(error) -> str:
     return jsonify({"error": "Forbidden"}), 403
 
 
-
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
