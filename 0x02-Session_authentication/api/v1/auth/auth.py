afab2ef1db4d744e4e6993a45d5ce3fd0a594e46
xilder <olabowaleayobami@gmail.com>
2024-07-10 10:26:02 +0100

    adds task 4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 
 from typing import List, TypeVar
 from flask import request
+import os
 
 
 class Auth:
@@ -46,3 +47,11 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """"returns None"""
         return None
+
+    def session_cookie(self, request=None):
+        """returns a cookie value from a request"""
+        if request is None:
+            return None
+
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
