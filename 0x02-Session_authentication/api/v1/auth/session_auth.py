5080d7e10ea9ca29bf42c4d44fb67bd6724ee6ac
xilder <olabowaleayobami@gmail.com>
2024-07-10 09:57:04 +0100

    adds task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -10,11 +10,17 @@ class SessionAuth(Auth):
 
     def create_session(self, user_id: str = None):
         """creates a Session ID for a user_id"""
-        if  user_id is None:
-            return None
-
-        if type(user_id) != str:
+        if  user_id is None or type(user_id) != str:
             return None
 
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+
+    def user_id_for_session_id(self, session_id: str = None):
+        """returns a user_id based on a session_id"""
+        if session_id is None:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
