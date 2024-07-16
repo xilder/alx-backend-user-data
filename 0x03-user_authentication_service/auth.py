cadcdabcebcb8a4c54c25254a5c41a993fa5f186
xilder <olabowaleayobami@gmail.com>
2024-07-16 23:46:12 +0100

    updates task 17

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -91,7 +91,8 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             self._db.update_user(
                 user_id=user.id,
-                hashed_password=_hash_password(password)
+                hashed_password=_hash_password(password),
+                reset_token=None
             )
         except NoResultFound:
             raise ValueError
