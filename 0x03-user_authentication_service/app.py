87248a8e0bee9c6281e0dbfea7285c01aa33a016
xilder <olabowaleayobami@gmail.com>
2024-07-16 23:53:36 +0100

    adds task 18

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -71,5 +71,17 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route("/reset_password" methods=["PUT"])
+def update_password():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    reset_token = request.form.get("reset_token")
+    try:
+        AUTH.update_password(reset_token, password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
