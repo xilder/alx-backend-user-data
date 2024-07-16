420d8abf9d0a3ddd118aca729693665260aa02b9
xilder <olabowaleayobami@gmail.com>
2024-07-16 22:26:06 +0100

    aa

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -25,6 +25,7 @@ def users():
     except Exception:
         return jsonify({"message": "email already registered"}), 400
 
+
 @app.route("/sessions", methods=["POST"])
 def login():
     email = request.form.get("email")
@@ -32,7 +33,7 @@ def login():
 
     if not (AUTH.valid_login(email, password)):
         abort(401)
-    else :
+    else:
         session_id = AUTH.create_session(email)
         response = jsonify({"email": email, "message": "logged in"})
         response.set_cookie("session_id", session_id)
