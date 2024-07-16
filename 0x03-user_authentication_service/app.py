255683cd09bf03b2cf3c409a23e6b9b1af5c23b0
xilder <olabowaleayobami@gmail.com>
2024-07-16 22:57:02 +0100

    adds task 15

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,5 +50,15 @@ def logout():
     return redirect(url_for("index"))
 
 
+@app.route("/profile", methods=["GET"])
+def profile():
+    try:
+        session_id = request.cookies.get("session_id")
+        user = AUTH.get_user_from_session_id(session_id)
+        return jsonify({"email": user.email}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
