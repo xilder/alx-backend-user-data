bc00f21cf45a3c5251cc49e3f9d103abea341553
xilder <olabowaleayobami@gmail.com>
2024-07-17 00:18:07 +0100

    ab

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -73,6 +73,7 @@ def get_reset_password_token() -> str:
 
 @app.route("/reset_password", methods=["PUT"])
 def update_password() -> str:
+    """updates a user password"""
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
     new_password = request.form.get("new_password")
