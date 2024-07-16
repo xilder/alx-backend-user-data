dd9f80110ea967b72164eaa3ff48bfb115fbf46a
xilder <olabowaleayobami@gmail.com>
2024-07-16 20:32:20 +0100

    adds task 4

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""auth module"""
+from bcrypt import hashpw, gensalt
+
+
+def _hash_password(password: str):
+    """hashes a passwrd and returns the value"""
+    hashed = hashpw(password.encode("utf-8"), gensalt())
+    return hashed
