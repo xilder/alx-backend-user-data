a1d76ef3a1edb73abbf92a519d987f967f695c48
xilder <olabowaleayobami@gmail.com>
2024-07-16 22:30:52 +0100

    adds task 12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,3 +57,13 @@ class Auth:
         session_id = _generate_uuid()
         user.session_id = session_id
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """gets a user by the session_id"""
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        return user
