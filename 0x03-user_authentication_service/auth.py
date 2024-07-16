b6d9a8ce438669bcab9df80a74e2e8ed2f4a8de0
xilder <olabowaleayobami@gmail.com>
2024-07-16 21:12:02 +0100

    updates task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,8 +24,7 @@ class Auth:
     def register_user(self, email: str, password: str) -> Union[User | None]:
         """registers a new user"""
         try:
-            self._db.find_user_by(email=email)
+            if self._db.find_user_by(email=email):
+                raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
-        else:
-            raise ValueError(f"User {email} already exists")
