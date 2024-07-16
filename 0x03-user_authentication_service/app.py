407a4bf8fce99d7263ef228f962990d52604e6d5
xilder <olabowaleayobami@gmail.com>
2024-07-16 21:42:19 +0100

    adds task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from auth import Auth
 
 app = Flask(__name__)
@@ -12,8 +12,17 @@ def index():
     """index route"""
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=["POST"])
+def users():
+    """users route"""
+    email = request.form.get("email")
+    password = request.form.get("password")
 
-
+    try:
+        user = AUTH.register_user(email=email, password=password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
