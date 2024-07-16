ced401beafa15779913e3c773a64155b58111255
xilder <olabowaleayobami@gmail.com>
2024-07-16 23:13:58 +0100

    updates task 15

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,8 +78,9 @@ class Auth:
     def get_reset_password_token(self, email: str) -> str:
         """generates a reset token"""
         try:
+            user = self._db.find_user_by(email=email)
             token = _generate_uuid()
-            self._db.update_user(reset_token=token)
+            self._db.update_user(user.id, reset_token=token)
             return token
         except Exception:
             raise ValueError
