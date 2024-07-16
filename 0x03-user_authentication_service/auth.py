69c6d4f559607a4d4940f7694308cd79833b5f2e
xilder <olabowaleayobami@gmail.com>
2024-07-16 21:09:25 +0100

    adds task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python3
 """auth module"""
+from typing import Union
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
 from bcrypt import hashpw, gensalt
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
     """hashes a passwrd and returns the value"""
     hashed = hashpw(password.encode("utf-8"), gensalt())
     return hashed
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        """Initialises the Auth class"""
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> Union[User | None]:
+        """registers a new user"""
+        try:
+            self._db.find_user_by(email=email)
+        except NoResultFound:
+            return self._db.add_user(email, _hash_password(password))
+        else:
+            raise ValueError(f"User {email} already exists")
