826d31615bf80b89b2e43a4c82b831e286b18973
xilder <olabowaleayobami@gmail.com>
2024-07-16 20:16:08 +0100

    adds task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -55,13 +55,9 @@ class DB:
     def update_user(self, user_id: int, **kwargs):
         """updates a user with updated values"""
         user = self.find_user_by(id=user_id)
-        if not user:
-            return None
-
         for k, v in kwargs.items():
             if not hasattr(user, k):
                 raise ValueError
             setattr(user, k, v)
         self._session.commit()
         return None
-
